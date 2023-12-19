5100fa483bdd1b3d06294894204a4d1668110fdb
Bryan Covell <bpc6@vt.edu>
2023-12-18 21:31:45 -0500

    jsbsim starts but errors trying to run script

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 int main() {
   // Create an instance of the flight dynamics model
   JSBSim::FGFDMExec exec;
-  //  exec.SetRootDir(SGPath("path/to/jsbsim"));
+  exec.SetRootDir(SGPath("third_party/jsbsim"));
 
-  exec.LoadScript(SGPath("third_party/jsbsim/scripts/c1723.xml"));
+  exec.LoadScript(SGPath("scripts/c1723.xml"));
   auto result = exec.Run();
   while (result) {        // cyclic execution
     result = exec.Run();  // execute JSBSim
