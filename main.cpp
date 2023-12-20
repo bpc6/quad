02b38f19184b9ecfbe699997a66324d018f942f5
Bryan Covell <bpc6@vt.edu>
2023-12-19 19:57:31 -0500

    load model and run sim

--- main.cpp
+++ main.cpp
@@ -2,20 +2,27 @@
 
 #include <iostream>
 
+#include "initialization/FGInitialCondition.h"
+
 int main() {
   JSBSim::FGFDMExec exec;
   exec.SetRootDir(SGPath("third_party/jsbsim/"));
   exec.SetAircraftPath(SGPath("aircraft/"));
   exec.SetEnginePath(SGPath("engine/"));
+  exec.SetSystemsPath(SGPath("systems/"));
   exec.LoadModel("F450");
 
-  //  int num_steps = 100;
-  //  exec.RunIC()
-  //  for (int i = 0; i < num_steps; ++i) {
-  //    exec.Run();
-  //    double time = exec.GetSimTime();
-  //    double altitude = exec.GetPropertyValue("position/h-sl-meters");
-  //    std::cout << time << ", " << altitude << std::endl;
-  //  }
+  auto fgic = exec.GetIC();
+  fgic->Load(SGPath("initGrnd.xml"));
+  exec.RunIC();
+
+  int num_steps = 10;
+  for (int i = 0; i < num_steps; ++i) {
+    exec.Run();
+    double time = exec.GetSimTime();
+    double altitude = exec.GetPropertyValue("position/h-sl-ft");
+    double lat = exec.GetPropertyValue("position/lat-geod-deg");
+    std::cout << time << ", " << altitude << ", " << lat << std::endl;
+  }
   return 0;
 }
