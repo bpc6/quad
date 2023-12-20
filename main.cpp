21c19329935eea3beb32e2c473f80d3d4c89220d
Bryan Covell <bpc6@vt.edu>
2023-12-19 23:11:12 -0500

    able to output to csv and maybe FlightGear

--- main.cpp
+++ main.cpp
@@ -10,19 +10,20 @@ int main() {
   exec.SetAircraftPath(SGPath("aircraft/"));
   exec.SetEnginePath(SGPath("engine/"));
   exec.SetSystemsPath(SGPath("systems/"));
+  exec.SetOutputDirectives(SGPath("output/OutputFG.xml"));
   exec.LoadModel("F450");
 
   auto fgic = exec.GetIC();
-  fgic->Load(SGPath("initGrnd.xml"));
+  fgic->Load(SGPath("reset00.xml"));
   exec.RunIC();
 
-  int num_steps = 10;
+  int num_steps = 10000;
   for (int i = 0; i < num_steps; ++i) {
     exec.Run();
     double time = exec.GetSimTime();
-    double altitude = exec.GetPropertyValue("position/h-sl-ft");
+    double altitude = exec.GetPropertyValue("position/h-agl-ft");
     double lat = exec.GetPropertyValue("position/lat-geod-deg");
-    std::cout << time << ", " << altitude << ", " << lat << std::endl;
+    //    std::cout << time << ", " << altitude << ", " << lat << std::endl;
   }
   return 0;
 }
