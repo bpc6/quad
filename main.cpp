5afcc721a553dc5c326f9075c19aa0a683e83497
bpc6 <35779213+bpc6@users.noreply.github.com>
2023-12-19 20:07:21 -0500

    get JSBSim running in main.cpp (#1)
    * add jsbsim as submodule
    
    * jsbsim build instructions
    
    * load model and run sim

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
+#include <FGFDMExec.h>
+
 #include <iostream>
 
+#include "initialization/FGInitialCondition.h"
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  JSBSim::FGFDMExec exec;
+  exec.SetRootDir(SGPath("third_party/jsbsim/"));
+  exec.SetAircraftPath(SGPath("aircraft/"));
+  exec.SetEnginePath(SGPath("engine/"));
+  exec.SetSystemsPath(SGPath("systems/"));
+  exec.LoadModel("F450");
+
+  auto fgic = exec.GetIC();
+  fgic->Load(SGPath("initGrnd.xml"));
+  exec.RunIC();
+
+  int num_steps = 10;
+  for (int i = 0; i < num_steps; ++i) {
+    exec.Run();
+    double time = exec.GetSimTime();
+    double altitude = exec.GetPropertyValue("position/h-sl-ft");
+    double lat = exec.GetPropertyValue("position/lat-geod-deg");
+    std::cout << time << ", " << altitude << ", " << lat << std::endl;
+  }
   return 0;
 }
