44c449f159675c1b4839d5aa483eb9bfe36011f7
Bryan Covell <bpc6@vt.edu>
2023-12-28 18:19:49 -0500

    trying to figure out how to make this drone do nothing

--- README.md
+++ README.md
@@ -3,10 +3,16 @@ Quadcopter simulation and control
 
 ## Installation & Setup
 ### Build JSBSim
-```
+```bash
 git submodule update --init --recursive
 cd third_party/jsbsim
 mkdir build && cd build
 cmake ..
 make
+```
+
+## Start Flightgear
+something like this
+```bash
+fgfs --fdm=--fdm=network,localhost,5502 --aircraft=ufo
 ```
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <FGFDMExec.h>
+#include <models/FGFCS.h>
+#include <models/FGInput.h>
 
 #include <iostream>
 
@@ -10,20 +12,32 @@ int main() {
   exec.SetAircraftPath(SGPath("aircraft/"));
   exec.SetEnginePath(SGPath("engine/"));
   exec.SetSystemsPath(SGPath("systems/"));
-  exec.SetOutputDirectives(SGPath("output/OutputFG.xml"));
+  exec.SetOutputDirectives(SGPath("output/OutputCSV.xml"));
+  //  exec.SetOutputDirectives(SGPath("output/OutputFG.xml"));
   exec.LoadModel("F450");
 
   auto fgic = exec.GetIC();
   fgic->Load(SGPath("reset00.xml"));
+
+  //  auto propMgr = exec.GetPropertyManager();
+  //  propMgr->GetNode()->SetDouble("fcs/cmdEscFR_nd", 0.0);
+  exec.SetPropertyValue("fcs/cmdEscFR_nd", 0.0);
+  exec.SetPropertyValue("fcs/cmdEscAL_nd", 0.0);
+  exec.SetPropertyValue("fcs/cmdEscFL_nd", 0.0);
+  exec.SetPropertyValue("fcs/cmdEscAR_nd", 0.0);
   exec.RunIC();
 
-  int num_steps = 10000;
-  for (int i = 0; i < num_steps; ++i) {
+  //  double altitude = exec.GetPropertyValue("position/h-agl-ft");
+  //  double lat = exec.GetPropertyValue("position/lat-geod-deg");
+
+  double max_t = 10.0;
+  for (double t = 0.0; t <= max_t;) {
     exec.Run();
+    t = exec.GetSimTime();
+    double cmd = exec.GetPropertyValue("fcs/cmdEscFR_nd");
     double time = exec.GetSimTime();
-    double altitude = exec.GetPropertyValue("position/h-agl-ft");
-    double lat = exec.GetPropertyValue("position/lat-geod-deg");
-    //    std::cout << time << ", " << altitude << ", " << lat << std::endl;
+    double alt = exec.GetPropertyValue("position/h-agl-ft");
+    //    std::cout << time << ", " << alt << ", " << cmd << std::endl;
   }
   return 0;
 }
